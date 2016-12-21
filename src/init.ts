04a0f1ea0f60ffee30a80462af22d8edaa987aa5
Mario Taferner <mario.taferner@gmail.com>
2016-12-21 17:45:59 +0100

    added foundation and todos

--- src/init.ts
+++ src/init.ts
@@ -1,12 +1,17 @@
 import $ = require('jquery');
+
 import { BarChart } from './plots/barchart';
 import { PieChart } from './plots/piechart';
 
 const $body = $('body');
 
 $body.html('Plots by d3 will be generated here.');
-$body.append('<div class="barchart"></div>')
-$body.append('<div class="piechart"></div>')
+$body.append('<div class="barchart"></div>');
+$body.append('<div class="piechart"></div>');
 
 const barchart = new BarChart('.barchart');
 const piechart = new PieChart('.piechart');
+
+// @todo embed css/style.css
+// @todo embed vendor/css/foundation.min.css
+// @todo embed vendor/js/foundation.min.js
