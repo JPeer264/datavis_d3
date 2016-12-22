e323704e4dfee04b71eb7ef89bc2021ede4612a7
JPeer264 <jan.oster94@gmail.com>
2016-12-22 15:16:15 +0100

    added:  * css support  * foundation  * some dependencies

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,6 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const webpack = require("webpack");
 
 module.exports = {
     entry: './src/init.ts',
@@ -7,13 +9,26 @@ module.exports = {
         filename: 'bundle.js'
     },
     resolve: {
-        extensions: ['', '.webpack.js', '.web.js', '.ts', '.js']
+        extensions: ['', '.webpack.js', '.web.js', '.ts', '.js', '.css']
     },
     module: {
         loaders: [{
             test: /\.ts$/,
             loader: 'ts-loader'
+        },{
+            test: /\.css$/,
+            loader: ExtractTextPlugin.extract("style-loader","css-loader")
         }]
     },
-    plugins: [new HtmlWebpackPlugin()]
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: 'Datavis',
+            template: 'src/index.ejs'
+        }),
+        // http://stackoverflow.com/questions/28969861/managing-jquery-plugin-dependency-in-webpack#answer-28989476
+        new webpack.ProvidePlugin({
+            $: "jquery",
+            jQuery: "jquery"
+        })
+    ]
 }
