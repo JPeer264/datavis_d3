89da526e1dcc729ca46c42306436b6208d49ade5
JPeer264 <jan.oster94@gmail.com>
2017-01-16 19:32:44 +0100

    deleted webpack generated js files

--- src/assets/data/options/index.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-var jsonData_1 = require("./jsonData");
-exports.jsonData = jsonData_1.jsonData;
-var chartOptions_1 = require("./chartOptions");
-exports.chartOptions = chartOptions_1.chartOptions;
--- src/plots/helper/index.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-var generateColorArray_1 = require("./generateColorArray");
-exports.generateColorArray = generateColorArray_1.generateColorArray;
-var getStackedNames_1 = require("./getStackedNames");
-exports.getStackedNames = getStackedNames_1.getStackedNames;
-var createTooltip_1 = require("./createTooltip");
-exports.createTooltip = createTooltip_1.createTooltip;
